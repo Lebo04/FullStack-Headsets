d1fecdd375238635c9f2484b45dfe7ee586f64aa
Lebo04 <nobomvukelebogile497@gmail.com>
2023-02-27 09:59:52 +0200

    Imports in index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const route = require("./controller");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const port = parseInt(process.env.PORT) || 3000;
+const app = express();
+const cookieParser = require("cookie-parser");
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.use(
+  route,
+  express.json,
+  express.urlencoded({ extended: false }),
+  cors({
+    origin: "",
+  }),
+  cookieParser()
+);
+
+// Server is running
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
