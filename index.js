a6cc76c68542784a9cec1df41b184c766d6363e5
Lebo04 <nobomvukelebogile497@gmail.com>
2023-02-27 10:58:39 +0200

    AuthenticatedUser

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const port = parseInt(process.env.PORT) || 3000;
 const app = express();
-const cookieParser = require("cookie-parser");
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
---
+++ model/index.js
@@ -0,0 +1,2 @@
+const db = require('../config');
+const { hash, compare, hashSync } = require('bcrypt');
