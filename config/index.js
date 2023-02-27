1a4d7b8e7d87cb45ea4ee085f293e31b39bb7916
Lebo04 <nobomvukelebogile497@gmail.com>
2023-02-27 09:49:38 +0200

    Database connection

---
+++ config/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { createPool } = require('mysql');
+
+let connection = createPool({
+    host: process.env.dbHost,
+    database: process.env.dbName,
+    password: process.env.dbPwd,
+    user: process.env.dbUser,
+    port: process.env.dbPort,
+    multipleStatements: true
+})
+
+module.exports = connection;
