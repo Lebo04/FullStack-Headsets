0f7f4a3f9ddb1f45629c8deee23d31d56441c6b9
unknown <aneeqabrown7@gmail.com>
2023-03-13 09:23:34 +0200

    update for firebase again

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
           <img alt="logo" src="../assets/CYpher.gif" style="max-width: 100%;">
           
   </div>
-  <h3 class="heading animate__animated animate__zoomInUp"> Welcome to Cypher, the home of the best quality headsets and computer mouses. Including affordable prices. We offer anime collaboration and big brands nation wide.</h3>
+  <h3 class="heading animate__animated animate__zoomInUp"> Welcome to Cypher, the home of the best quality headsets and computer mouses. Including affordable prices. We offer anime collaboration and big brands nation wide...</h3>
 </template>
 
 
